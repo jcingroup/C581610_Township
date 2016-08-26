882f3ae435b0e9b72d0348d081f5e6eb11a91b22
Ajoe <sdec0817@gmail.com>
2016-08-26 18:56:22 +0800

    + resident 相關 檔案

--- Work.WebProj/Scripts/src/action_type.ts
+++ Work.WebProj/Scripts/src/action_type.ts
@@ -1,1 +1,14 @@
-﻿
+﻿export const ac_type_comm = {
+    load: 'load', //進人報表 一般為ajax調用
+    grid: 'grid', //Gird List 狀態
+    add: 'add', //新增編輯狀態
+    update: 'update', //修改狀態
+    chg_fld_val: 'change_field_value',
+    cancel: 'cancel', //取消
+    load_report_item: 'load_report_item',
+    loading_on: 'visible_on',
+    loading_off: 'visible_off',
+    chg_cust_id: 'chg_cust_id',
+    set_query_master: 'set_query_master'
+
+}
