889c24a93a3464d0340ccb70e3b84014819b94ad
Ajoe <sdec0817@gmail.com>
2016-08-29 01:19:58 +0800

    刪除 無用 tsx

--- Work.WebProj/Scripts/src/tsx/w-sell-list.tsx
+++
@@ -1,8 +0,0 @@
-﻿import React = require('react');
-import ReactDOM = require('react-dom');
-import CommMatter = require('comm-matter');
-
-declare var community_id: number;
-declare var info_type: string;
-var dom = document.getElementById('content');
-ReactDOM.render(<CommMatter.MatterList community_id={community_id} info_type={info_type} />, dom); 
