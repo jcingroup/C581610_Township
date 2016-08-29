f815631158b16001e212b228a00766c2e7200bf2
Ajoe <sdec0817@gmail.com>
2016-08-29 12:36:24 +0800

    前台 編輯器套程式

--- Work.WebProj/Controllers/GroupController.cs
+++ Work.WebProj/Controllers/GroupController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Web.Mvc;
 using DotWeb.Controller;
+using System.Linq;
+using ProcCore.Business.DB0;
 
 namespace DotWeb.WebApp.Controllers
 {
@@ -8,7 +10,33 @@ namespace DotWeb.WebApp.Controllers
         // GET: Group
         public ActionResult Index()
         {
-            return View("Group");
+            m_Editor item = new m_Editor();
+            using (var db0 = getDB0())
+            {
+                #region get content
+
+                item = db0.Editor
+                    .Where(x => !x.i_Hide & x.editor_id == (int)EditorState.Group)
+                    .OrderByDescending(x => x.sort)
+                    .Select(x => new m_Editor()
+                    {
+                        id = x.editor_id,
+                        name = x.name,
+                        UpdateDatetime = x.i_UpdateDateTime,
+                        detail = x.EditorDetail
+                                .OrderByDescending(y => y.sort)
+                                .Select(y => new m_EditorDetail()
+                                {
+                                    id = y.editor_detail_id,
+                                    name = y.detail_name,
+                                    sort = y.sort,
+                                    content = y.detail_content
+                                })
+                    }).FirstOrDefault();
+
+                #endregion
+            }
+            return View("Group", item);
         }
     }
 }
