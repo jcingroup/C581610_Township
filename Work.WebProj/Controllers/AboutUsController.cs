88315c829bae451c08c5ff8010c1defa0a4b53d1
Ajoe <sdec0817@gmail.com>
2016-08-29 13:27:25 +0800

    前台 編輯器調整

--- Work.WebProj/Controllers/AboutUsController.cs
+++ Work.WebProj/Controllers/AboutUsController.cs
@@ -12,48 +12,8 @@ namespace DotWeb.WebApp.Controllers
         // GET: AboutUs
         public ActionResult Index()
         {
-            m_Editor item = new m_Editor();
-            using (var db0 = getDB0())
-            {
-                #region get content
-
-                item = db0.Editor
-                    .Where(x => !x.i_Hide & x.editor_id == (int)EditorState.AboutUs)
-                    .OrderByDescending(x => x.sort)
-                    .Select(x => new m_Editor()
-                    {
-                        id = x.editor_id,
-                        name = x.name,
-                        UpdateDatetime = x.i_UpdateDateTime,
-                        detail = x.EditorDetail
-                                .OrderByDescending(y => y.sort)
-                                .Select(y => new m_EditorDetail()
-                                {
-                                    id = y.editor_detail_id,
-                                    name = y.detail_name,
-                                    sort = y.sort,
-                                    content = y.detail_content
-                                })
-                    }).FirstOrDefault();
-
-                #endregion
-            }
+            CategoryL1Data item = getEditorData((int)EditorState.AboutUs);
             return View("AboutUs", item);
         }
-    }
-
-    public class m_EditorDetail
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public int? sort { get; set; }
-        public string content { get; set; }
-    }
-    public class m_Editor
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public DateTime? UpdateDatetime { get; set; }
-        public IEnumerable<m_EditorDetail> detail { get; set; }
     }
 }
