6a293b02c1e81d231d857ba1cf9e8d36e7da3c33
TinWu <yyuuix@gmail.com>
2016-08-30 17:13:58 +0800

    會員修改

---
+++ Work.WebProj/Controllers/MemberController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotWeb.Controller;
+
+namespace DotWeb.WebApp.Controllers
+{
+    public class MemberController : WebUserController
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View("Edit");
+        }
+    }
+}
